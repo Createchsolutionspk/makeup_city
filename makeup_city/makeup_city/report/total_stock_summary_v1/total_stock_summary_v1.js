83c4a58eda8624c0a35b434fc46e30cc73c566bd
shahSpider <aq.syed007@gmail.com>
2026-03-04 12:48:15 +0500

    feat: add new report Total Stock Summary V1 to show zero stock

---
+++ makeup_city/makeup_city/report/total_stock_summary_v1/total_stock_summary_v1.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2026, Createch Solutions and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Total Stock Summary V1"] = {
+	filters: [
+		{
+			fieldname: "group_by",
+			label: __("Group By"),
+			fieldtype: "Select",
+			width: "80",
+			reqd: 1,
+			options: ["Warehouse", "Company"],
+			default: "Warehouse",
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			width: "80",
+			options: "Company",
+			reqd: 1,
+			default: frappe.defaults.get_user_default("Company"),
+			depends_on: "eval: doc.group_by != 'Company'",
+		},
+		{
+			fieldname: "show_zero_stock",
+			label: __("Show Zero Stock"),
+			fieldtype: "Check",
+			width: "80"
+		}
+	],
+};
